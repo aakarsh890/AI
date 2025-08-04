5dc4b203654a799075fac6fdb13dd2158246cf1e
aakarsh890 <162731300+aakarsh890@users.noreply.github.com>
2025-08-05 00:10:41 +0530

    updated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import rediClient from "../config/redis.js";
 import User from "../models/user.model.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import validateUserInput from "../utils/validator.js";
 
